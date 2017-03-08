f3a606aa8713c2c0a684b67198129201d8280259
Guilherme Santos <sntguilherme@gmail.com>
2017-03-08 10:23:57 -0300

    Corrigindo as renderizações das propriedades do Book

--- src/app/components/BookCard.js
+++ src/app/components/BookCard.js
@@ -14,17 +14,22 @@ class BookCard extends React.Component {
 
   render() {
     const {book} = this.props;
+    const {title, publisher, publishedDate, imageLinks} = book.volumeInfo;
+    const bookCardSubtitle = (publisher || '') + (publishedDate ? ' (' + publishedDate + ')' : '');
+    const cardHeader = {title, subtitle: bookCardSubtitle};
+    if (imageLinks) {
+      cardHeader.avatar = imageLinks.thumbnail;
+    }
+
     return (
       <li className="list-books__book-card">
         <Card onClick={this.handleBookCardClick}>
           <CardHeader
-            title={book.volumeInfo.title}
-            subtitle={book.volumeInfo.publisher + ' (' + book.volumeInfo.publishedDate + ')'}
-            avatar={book.volumeInfo.imageLinks.thumbnail}
+            {...cardHeader}
             />
-          <CardText>
-            {book.searchInfo.textSnippet}
-          </CardText>
+          {book.searchInfo &&
+            <CardText>{book.searchInfo.textSnippet}</CardText>
+          }
         </Card>
       </li>
     );
--- src/app/components/BookDetails.js
+++ src/app/components/BookDetails.js
@@ -2,43 +2,61 @@ import React, {PropTypes} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import ActionOpenInNew from 'material-ui/svg-icons/action/open-in-new';
 
+const renderBookAtuthors = authors => (
+  <div>
+    <label>Autores</label>
+    {authors.map(author =>
+      <span
+        key={author}
+        >
+        {author}
+      </span>
+    )}
+  </div>
+);
+
+const renderDeatail = (label, value) => (
+  <div className="book-details__item">
+    <label>{label}</label>
+    <span>{value}</span>
+  </div>
+);
+
+const renderButtonOpenBook = link => (
+  <div className="book-details__open-content-container">
+    <RaisedButton
+      label="Ver conteúdo..."
+      labelPosition="before"
+      icon={<ActionOpenInNew/>}
+      href={link}
+      target="_blank"
+      />
+  </div>
+);
+
 const BookDetails = ({book}) => (
   <div className="book-details">
     <h1>{book.volumeInfo.title}</h1>
 
-    <div>
-      <label>Autores</label>
-      {book.volumeInfo.authors.map(author =>
-        <span key={author}>
-          {author}
-        </span>
-      )}
-    </div>
+    {book.volumeInfo.authors &&
+      renderBookAtuthors(book.volumeInfo.authors)
+    }
 
-    <div>
-      <label>Editora</label>
-      <span>{book.volumeInfo.publisher}</span>
-    </div>
+    {book.volumeInfo.publisher &&
+      renderDeatail('Editora', book.volumeInfo.publisher)
+    }
 
-    <div>
-      <label>Publicado em</label>
-      <span>{book.volumeInfo.publishedDate}</span>
-    </div>
+    {book.volumeInfo.publishedDate &&
+      renderDeatail('Publicado em', book.volumeInfo.publishedDate)
+    }
 
-    <div>
-      <label>Descrição</label>
-      <p>{book.volumeInfo.description}</p>
-    </div>
+    {book.volumeInfo.description &&
+      renderDeatail('Descrição', book.volumeInfo.description)
+    }
 
-    <div className="book-details__open-content-container">
-      <RaisedButton
-        label="Ver conteúdo"
-        labelPosition="before"
-        icon={<ActionOpenInNew/>}
-        href={book.selfLink}
-        target="_blank"
-        />
-    </div>
+    {book.selfLink &&
+      renderButtonOpenBook(book.selfLink)
+    }
   </div>
 );
 
